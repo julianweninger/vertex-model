a59c580adaad079f84c35eebed8dc93681642db4
Julian Weninger <julian.weninger@unige.ch>
2020-04-15 21:22:27 +0200

    Reactivate test_transition

--- src/models/PCPVertex/test/test_transition.cc
+++ src/models/PCPVertex/test/test_transition.cc
@@ -1,192 +1,266 @@
-#define BOOST_TEST_MODULE PCPVertexTestTransition
+#define BOOST_TEST_MODULE PCPVertexTestNew
 
 #include <assert.h>
 #include <iostream>
 #include <boost/test/unit_test.hpp>
 
-#include "utils.hh"
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
 
+#include "../PCPVertex.hh"
+#include "../energy.hh"
+#include "../algorithm.hh"
+#include "../operations.hh"
+#include "../PCPVertex_write_tasks.hh"
 
-template<bool periodic_bc>
-void test_T1_transition (std::string cfg)
-{
-    using Edge = typename PCPVertex<periodic_bc>::EdgeNew;
+using namespace Utopia::Models::PCPVertex;
+using Utopia::Models::PCPVertex::DataIO::time_energy_adaptor;
 
-    // auto model_cfg = model.get_cfg();
+PCPVertex model_factory(bool periodic) {
+    using namespace Utopia::Models::PCPVertex::DataIO;
 
-    // const auto& am = model.get_am();
-    // const auto& vertices = am.vertices();
-    // const auto& edges = am.edges();
-    // const auto& cells = am.cells();
-
-    // // pick some inner edge
-    // std::weak_ptr<Edge> e = edges[edges.size() / 3];
-
-    // // contract this inner edge
-    // e.lock()->state.linetension = 20.;
-
-    // run the model
-
-    // // check that the edge e has been removed
-    // BOOST_TEST(e.expired());
-    
-    // // check that the number of egdes did not change
-    // BOOST_TEST(am.edges().size() == edges.size());
-    // BOOST_TEST(am.cells().size() == cells.size());
-    // BOOST_TEST(am.vertices().size() == vertices.size());
+    if (periodic) {
+        Utopia::PseudoParent pp("test_periodic.yml");
+        return PCPVertex("PCPVertex", pp);
+    }
+    else {
+        Utopia::PseudoParent pp("test.yml");
+        return PCPVertex("PCPVertex", pp);
+    }
 }
 
-BOOST_FIXTURE_TEST_CASE(T1_transition_periodic, ModelFixture)
-{
-    model.run();
 
-    // test_T1_transition<true>("test_periodic.yml");
-}
+/// A fixture used in the test_PCPVertex test suite
+/** Besides destructing the model it is also necessary to destruct the logger
+ *  and to free the datapath to construct a new model from the same config
+ *
+ *  NOTE the models output_path needs to be "test_data.h5"
+ *
+ */
+struct ModelFixture {
+    std::shared_ptr<spdlog::logger> log;
 
-// BOOST_FIXTURE_TEST_CASE(T1_transition_non_periodic, ModelFixture)
-// {
-//     test_T1_transition<false>("test_non_periodic.yml");
-// }
+    // Construct
+    ModelFixture ()
+    :
+        log([]() {
+            auto logger = spdlog::get("test");
+
+            // Create it only if it does not already exist
+            if (not logger) {
+                logger = spdlog::stdout_color_mt("test");
+            }
+
+            // Set level and global logging pattern
+            logger->set_level(spdlog::level::debug);
+            spdlog::set_pattern("[%T.%e] [%^%l%$] [%n]  %v");
+            // "[HH:MM:SS.mmm] [level(colored)] [logger]  <message>"
+
+            return logger;
+        }())
+    { }
+
+    // Teardown, invoked after each test
+    ~ModelFixture () {
+        log->info("Tearing down ...");
+        std::remove("test_data.h5");
+        spdlog::drop_all();
+    }
+};
 
 BOOST_FIXTURE_TEST_SUITE (test_PCPVertex_transitions, ModelFixture)
 
+    void test_custom_links(PCPVertex model) {
+        const auto& am = model.get_am();
 
+        for (auto v : am.vertices()) {
+            auto adj_edges = am.adjoint_edges_of(v);
+            BOOST_TEST(adj_edges.size() == 3);
+            for (auto e : adj_edges) {
+                BOOST_TEST(e);
+            }
 
-template<bool periodic_bc>
-void test_T2_transition (std::string cfg)
-{
-    auto model = model_factory<periodic_bc>(cfg);
-    auto model_cfg = model.get_cfg();
-
-    auto cells = model.get_cells();
-    auto vertices = model.get_vertices();
-    auto edges = model.get_edges();
-
-    // pick some inner cell
-    auto c = cells[cells.size() / 2 + 1];
-    auto num_vertices = c.lock()->vertices.size();
-    auto num_edges = c.lock()->edges_ordered.size();
-
-    // contract this inner cell
-    c.lock()->contractility = 1.;
-    c.lock()->area_preferential = 0.;
-
-    // run the model
-    model.run();
-
-    // check that the edge e has been removed
-    BOOST_TEST(c.expired());
-
-    // check that the number of objects changed accordingly
-    BOOST_TEST(model.get_cells().size() == cells.size() - 1);
-    BOOST_TEST(model.get_vertices().size() == vertices.size() - 2);
-    BOOST_TEST(model.get_edges().size() == edges.size() - 3);
-    
-    test_weak_links(model);
-
-    destruct_model_factory(model);
-}
-
-BOOST_AUTO_TEST_CASE(T2_transition_periodic)
-{
-    // test_T2_transition<true>("test_periodic.yml");
-}
-
-BOOST_AUTO_TEST_CASE(T2_transition_non_periodic)
-{
-    // test_T2_transition<false>("test_non_periodic.yml");
-}
-
-
-
-template<bool periodic_bc>
-void test_cell_division (std::string cfg)
-{
-    std::vector<int> test_cases;
-    if constexpr (periodic_bc) {
-        test_cases = {15, 1, 4, 5};
-    }
-    else { test_cases = {5}; }
-
-    for (int cell_pos : test_cases) {
-    auto model = model_factory<periodic_bc>(cfg);
-
-    auto vertices = model.get_vertices();
-    auto edges = model.get_edges();
-    auto cells = model.get_cells();
-
-    model.divide_cell(cells[cell_pos].lock(), 0.); 
-    // this is a cell not at the boundary
-
-
-    auto new_vertices = model.get_vertices();
-    auto new_edges = model.get_edges();
-    auto new_cells = model.get_cells();
-
-    // assert that the right number of objects has been added during division
-    assert(cells.size() == new_cells.size()-1);
-    assert(edges.size() == new_edges.size()-3);
-    assert(vertices.size() == new_vertices.size()-2);
-
-    for (auto v : new_vertices) {
-        assert(not v.expired());
-        assert(not v.lock()->remove);            
-    }
-    for (auto e : new_edges) {
-        assert(not e.expired());
-        assert(not e.lock()->remove);
-    }
-    for (auto c : new_cells) {
-        assert(not c.expired());
-        assert(not c.lock()->remove);
-    }
-
-    int cnt_expired = 0;
-    for (auto c : cells) {
-        cnt_expired += c.expired();
-    }
-    assert(cnt_expired == 1);
-    cnt_expired = 0;
-    for (auto e : edges) {
-        cnt_expired += e.expired();
-    }
-    assert(cnt_expired == 2);
-    cnt_expired = 0;
-    for (auto v : vertices) {
-        cnt_expired += v.expired();
-    }
-    assert(cnt_expired == 0);
-
-    if constexpr (periodic_bc)
-    {
-        for (auto v : new_vertices) {
-            assert(v.lock()->adj_edges.size() == 3);
-            assert(v.lock()->adj_cells.size() == 3);
+            auto adj_cells = am.adjoint_cells_of(v);
+            BOOST_TEST(adj_cells.size() == 3);
+            for (auto c : adj_cells) {
+                BOOST_TEST(c);
+            }
         }
-        for (auto e : new_edges) {
-            assert(not e.lock()->adj_cell_a.expired() and
-                   not e.lock()->adj_cell_b.expired());
+        for (auto e : am.edges()) {
+            BOOST_TEST(e->custom_links().a);
+            BOOST_TEST(e->custom_links().b);
+
+            auto adj_edges = am.adjoint_edges_of(e->custom_links().a);
+            BOOST_TEST((std::find(adj_edges.begin(), adj_edges.end(), e) != 
+                       adj_edges.end()));
+            adj_edges = am.adjoint_edges_of(e->custom_links().b);
+            BOOST_TEST((std::find(adj_edges.begin(), adj_edges.end(), e) != 
+                       adj_edges.end()));
+
+            auto [adj_a, adj_b] = am.adjoints_of(e);
+            BOOST_TEST(adj_a);
+            BOOST_TEST(adj_b);
         }
+        for (auto c : am.cells()) {
+            BOOST_TEST(am.area_of(c) > 0);
+
+            const auto& vertices = c->custom_links().vertices;
+            const auto& edges = c->custom_links().edges;
+            for (auto v : vertices) {
+                BOOST_TEST(v);
+            }
+
+            BOOST_TEST(vertices.size() == edges.size());
+            BOOST_TEST(vertices.size() >= 3);
+            
+            auto [e, flip] = edges[0];
+            auto first = e->custom_links().a;
+            auto iterator = e->custom_links().b;
+            if (flip) {
+                std::swap(first, iterator);
+            }
+
+            for (int i = 1; i < edges.size(); i++) {
+                auto [e, flip] = edges[i];
+                BOOST_TEST(e);
+
+                if (not flip) {
+                    BOOST_TEST(iterator == e->custom_links().a);
+                    iterator = e->custom_links().b;
+                }
+                else {
+                    BOOST_TEST(iterator == e->custom_links().b);
+                    iterator = e->custom_links().a;
+                }
+            }
+            BOOST_TEST(iterator == first);
+        }
+
+        const auto& cells = am.cells();
+        const auto& edges = am.edges();
+        const auto& vertices = am.vertices();
     }
 
-    model.run();
+    void test_divide_cell(bool periodic) {
+        auto model = model_factory(periodic);
 
-    test_weak_links(model);
-    
-    destruct_model_factory(model);
+        const auto& am = model.get_am();
+
+        const auto& cells = am.cells();
+        const auto& edges = am.edges();
+        const auto& vertices = am.vertices();
+
+        int num_cells = cells.size();
+        int num_edges = edges.size();
+        int num_vertices = vertices.size();
+
+        auto cell = cells[cells.size() / 2];
+        model.divide_cell(cell, 0.);
+
+        BOOST_TEST(cells.size() == num_cells + 1);
+        BOOST_TEST(edges.size() == num_edges + 3);
+        BOOST_TEST(vertices.size() == num_vertices + 2);
+
+        test_custom_links(model);
+
+        model.run();
     }
-}
 
-BOOST_AUTO_TEST_CASE(cell_division_non_periodic)
-{
-    // test_cell_division<false>("test_non_periodic.yml");
-}
+    // BOOST_AUTO_TEST_CASE(test_divide_cell_non_periodic) {
+    //     test_divide_cell(false);
+    // }
 
-BOOST_AUTO_TEST_CASE(cell_division_periodic)
-{
-    // test_cell_division<true>("test_periodic.yml");
-}
+    BOOST_AUTO_TEST_CASE(test_divide_cell_periodic) {
+        test_divide_cell(true);
+    }
+
+    void test_T1_transition(bool periodic) {
+        auto model = model_factory(periodic);
+        model.prolog();
+
+        const auto& am = model.get_am();
+
+        const auto& cells = am.cells();
+        const auto& edges = am.edges();
+        const auto& vertices = am.vertices();
+
+        int num_cells = cells.size();
+        int num_edges = edges.size();
+        int num_vertices = vertices.size();
+
+        auto edge = edges[edges.size() / 3];
+        int use_cnt = edge.use_count();
+        edge->state.linetension = 200.;
+
+        auto a = edge->custom_links().a;
+        auto b = edge->custom_links().b;
+
+        PCPVertex::SpaceVec center = .5*(am.position_of(a) + am.position_of(b));
+        am.move_to(a, 0.999*center);
+        am.move_to(b, center);
+
+        model.iterate();
+
+        BOOST_TEST(edge.use_count() == 1);
+
+        BOOST_TEST(cells.size() == num_cells);
+        BOOST_TEST(edges.size() == num_edges);
+        BOOST_TEST(vertices.size() == num_vertices);
+
+        test_custom_links(model);
+    }
+
+    // BOOST_AUTO_TEST_CASE(test_T1_non_periodic) {
+    //     test_T1_transition(false);
+    // }
+
+    BOOST_AUTO_TEST_CASE(test_T1_periodic) {
+        test_T1_transition(true);
+    }
+
+    void test_T2_transition(bool periodic) {
+        auto model = model_factory(periodic);
+        model.prolog();
+        
+        const auto& am = model.get_am();
+
+        const auto& cells = am.cells();
+        const auto& edges = am.edges();
+        const auto& vertices = am.vertices();
+
+        int num_cells = cells.size();
+        int num_edges = edges.size();
+        int num_vertices = vertices.size();
+
+        auto cell = cells[cells.size() / 2];
+        int use_cnt = cell.use_count();
+
+        cell->state.area_preferential = 0.;
+        for (int i = 0; i < 1000; i++) {
+            model.iterate();
+            if (cells.size() > num_cells) { 
+                break;
+            }
+        }
+
+        BOOST_TEST(cell.use_count() == 1);
+
+        BOOST_TEST(cells.size() == num_cells - 1);
+        BOOST_TEST(edges.size() == num_edges-cell->custom_links().edges.size());
+        BOOST_TEST(vertices.size() == 
+                   num_vertices - cell->custom_links().vertices.size() + 1);
+
+        test_custom_links(model);
+    }
+
+    // BOOST_AUTO_TEST_CASE(test_T2_non_periodic) {
+    //     test_T2_transition(false);
+    // }
+
+    BOOST_AUTO_TEST_CASE(test_T2_periodic) {
+        test_T2_transition(true);
+    }
+
+
 
 BOOST_AUTO_TEST_SUITE_END()
-
-
